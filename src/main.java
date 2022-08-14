c02aaaf0c5dd84af4f60748fc0101006c6186abc
xerias <marcinn.smulski@gmail.com>
2022-08-14 20:05:27 +0200

    Facility: setting user difficulty function with user reponse added. Scanners used, Loop

--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 
 
 public class main {
@@ -26,7 +28,35 @@ public class main {
             e.printStackTrace();
         }
 
+        difficulty=setDiffculty(); // with user response
 
+    }
+
+    private Difficulty setDiffculty() {
+        boolean loop=true;
+        Scanner input=new Scanner(System.in);
+        int guess_cnt=0;
+        System.out.println("Choose difficulty. Type easy (4 pairs) or hard (8 pairs)");
+        while(loop){
+            String response=input.nextLine();
+            if(response.toLowerCase(Locale.ROOT).equals("easy")) {
+                //----------------------------------------------------------------------------------------------;
+                return Difficulty.EASY;
+            }else{
+                if(response.toLowerCase(Locale.ROOT).equals("hard")){
+                    //------------------------------------------------------------------------------------------;
+                    return Difficulty.HARD;
+                }else{
+                    guess_cnt++;
+                    if (guess_cnt<2) System.out.println("Not recognized.Try again");
+                    else{
+                        System.out.println("Sorry, can't recognize Your reply. Game level set to EASY");
+                        loop=false;
+                    }
+                }
+            }
+        }
+        return Difficulty.EASY;
     }
 
     ArrayList <String> getWordsArray(String path) throws  IOException {
