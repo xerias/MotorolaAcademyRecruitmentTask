9f993da885c14ac1d680181e2dc03e812a5db298
xerias <marcinn.smulski@gmail.com>
2022-08-14 19:45:32 +0200

    Facility: reading date from words.txt into Arraylist added. FileInputStream used

--- src/main.java
+++ src/main.java
@@ -1,3 +1,5 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -16,6 +18,37 @@ public class main {
         ArrayList<String> words;
         //String absolutePathtoFile = "C:\\Users\\Dell\\Documents\\Motorola - recruitment task Java\\words.txt";
         String relativePathToFile = "words.txt";
+
+        try {
+            words = getWordsArray(relativePathToFile);
+            System.out.println(words);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    ArrayList <String> getWordsArray(String path) throws  IOException {
+        FileInputStream fis= new FileInputStream(path);
+        ArrayList <String> words=new ArrayList<String>();
+        int c;
+        c= fis.read();
+        if(c==-1) return null;
+        //-------------------------------------------------------
+        String w ="";
+        do{
+            if(c=='\r'){
+                words.add(w);
+                fis.skip(1); // ommiting "\n" character
+                w="";
+            }else{
+                w+=(char)c;
+
+            }
+            c= fis.read();
+        } while (c!=-1);
+        return words;
     }
 }
 
