99841f7d5b290f22f0e340fd96516b433c7321d3
xerias <marcinn.smulski@gmail.com>
2022-08-14 19:24:21 +0200

    initial commit. Main created. Relative/absolute path is set. Difficualty in form ENUM.

---
+++ src/main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+
+public class main {
+    public static void main(String[] args) {
+        new main();
+    }
+
+    enum Difficulty {
+        EASY,
+        HARD
+    }
+
+    public main() {
+        Difficulty difficulty = null;
+        ArrayList<String> words;
+        //String absolutePathtoFile = "C:\\Users\\Dell\\Documents\\Motorola - recruitment task Java\\words.txt";
+        String relativePathToFile = "words.txt";
+    }
+}
+
+
