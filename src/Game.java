a54ee147efb4018de11eab9d1863883c647a760d
xerias <marcinn.smulski@gmail.com>
2022-08-15 04:45:05 +0200

    basic app. 1 game without loop. functionality of Gmae object slightlz changed, Words of current game progress, answers, and the initial state when are field are covered are stored in memory and initzialized once

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,20 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Locale;
+
+import static java.lang.Character.getNumericValue;
 
 public class Game {
     private
-    String[][] result;
+    String[][] x_table;
+    String[][] answers;
+    String[][] gameTable;
     String[] A;
     String[] B;
     ArrayList<String> words;
     private int level; // 4 or 8, easy or hard
     private int score;
-    private int attempts;
+    private int attemptsLeft;
     private int time;
 
     public Game(ArrayList<String> words,int level){
@@ -33,25 +38,39 @@ public class Game {
             A = this.words.toArray(new String[0]);
             Collections.shuffle(this.words);
             B = this.words.toArray(new String[0]);
+            attemptsLeft =DIFFICULTY.EASY_ATTEMPTS;
 
-            attempts=DIFFICULTY.EASY_ATTEMPTS;
             this.level=level;
-            attempts= level==DIFFICULTY.EASY ? DIFFICULTY.EASY_ATTEMPTS : DIFFICULTY.HARD_ATTEMPTS;
-            this.result = new String[3][level+1];
-            result [0][0]=" ";
-            for(int i=1;i<=level;i++) result[0][i]=Integer.toString(i);
-            result[1][0]="A";
-            result[2][0]="B";
+            attemptsLeft = level==DIFFICULTY.EASY ? DIFFICULTY.EASY_ATTEMPTS : DIFFICULTY.HARD_ATTEMPTS;
+            this.x_table = new String[3][level+1];
+            this.answers= new String[3][level+1];
+            this.gameTable= new String[3][level+1];
+            gameTable[0][0]=answers[0][0]= x_table[0][0]=" ";
+
+            for(int i=1;i<=level;i++) answers[0][i]= x_table[0][i]=Integer.toString(i);
+            gameTable[1][0]=answers[1][0]= x_table[1][0]="A";
+            gameTable[2][0]=answers[2][0]= x_table[2][0]="B";
+
             for(int i=1;i<=level;i++){
-            int length = Math.max(A[i-1].length(),B[i-1].length());
-            StringBuilder sb= new StringBuilder(length);
-            sb.append(i);
-            sb.append(new String (new char[length-1]).replace("\0"," "));
-            result[0][i]=sb.toString();
-            sb.replace(0,1,"X");
-            result[1][i]=sb.toString();
-            result[2][i]=sb.toString();
-        }
+                int length = Math.max(A[i-1].length(),B[i-1].length());
+                //coordinates builder
+                StringBuilder sb= new StringBuilder(length);
+                sb.append(i);
+                sb.append(new String (new char[length-1]).replace("\0"," "));
+                gameTable[0][i]=x_table[0][i]=sb.toString();
+                gameTable[0][i]=answers[0][i]=sb.toString();
+
+                //X...builder
+                sb.replace(0,1,"X");
+                gameTable[1][i]=answers[1][i]= x_table[1][i]=sb.toString();
+                gameTable[2][i]=answers[2][i]= x_table[2][i]=sb.toString();
+
+                //words from A and B, added whitespaces. Length adjusted to the highest in column
+                answers[1][i]=String.join("",A[i-1],new String(new char[length-A[i-1].length()]).replace("\0"," "));
+                answers[2][i]=String.join("",B[i-1],new String(new char[length-B[i-1].length()]).replace("\0"," "));
+
+            }
+
     }
 
     public int getScore() {
@@ -67,12 +86,16 @@ public class Game {
         throw new RuntimeException("variable is already set");
     }
 
-    public int getAttempts() {
-        return attempts;
+    public int getAttemptsLeft() {
+        return attemptsLeft;
     }
 
-    public void setAttempts(int attempts) {
-        this.attempts = attempts;
+    public void setAttemptsLeft(int attemptsLeft) {
+        this.attemptsLeft = attemptsLeft;
+    }
+
+    public  void decreaseAttemptsLeft () {
+        this.attemptsLeft-=1;
     }
 
     public int getTime() {
@@ -84,14 +107,87 @@ public class Game {
     }
 
     public void printResult(){
+        System.out.println("-------------------------------------");
+        String mode = level==DIFFICULTY.EASY ? "easy" : "hard";
+        System.out.println("Level: "+mode);
+        System.out.println("Guess chances: "+attemptsLeft+"\n");
         String s="";
         for(int i=0;i<3;i++){
             for(int k=0;k<level+1;k++){
-                s+=result[i][k];
+                s+= x_table[i][k];
                 s+=" ";
             }
             System.out.println(s);
             s="";
         }
+        System.out.println("-------------------------------------");
+    }
+    public void printAnswers(){
+        System.out.println("-------------------------------------");
+        String mode = level==DIFFICULTY.EASY ? "easy" : "hard";
+        System.out.println("Level: "+mode);
+        System.out.println("Guess chances: "+attemptsLeft+"\n");
+        String s="";
+        for(int i=0;i<3;i++){
+            for(int k=0;k<level+1;k++){
+                s+=answers[i][k];
+                s+=" ";
+            }
+            System.out.println(s);
+            s="";
+        }
+        System.out.println("-------------------------------------");
+    }
+
+    public void printGameTable(){
+        System.out.println("-------------------------------------");
+        String mode = level==DIFFICULTY.EASY ? "easy" : "hard";
+        System.out.println("Level: "+mode);
+        System.out.println("Guess chances: "+attemptsLeft+"\n");
+        String s="";
+        for(int i=0;i<3;i++){
+            for(int k=0;k<level+1;k++){
+                s+=gameTable[i][k];
+                s+=" ";
+            }
+            System.out.println(s);
+            s="";
+        }
+        System.out.println("-------------------------------------");
+    }
+
+
+    public void cover(String coordinates){
+        int i = coordinates.charAt(0) == 'A' ? 1 : 2;
+        int pos = getNumericValue(coordinates.charAt(1));
+        gameTable[i][pos]=x_table[i][pos];
+
+    }
+
+    public String uncover (String coordinates){
+        System.out.println(coordinates);
+        int i = coordinates.charAt(0) == 'A' ? 1 : 2;
+        int pos = getNumericValue(coordinates.charAt(1));
+        gameTable[i][pos]=answers[i][pos];
+        return gameTable[i][pos];
+    }
+
+    boolean check (String cor1, String cor2){
+        boolean result=false;
+        int i1 = cor1.charAt(0) == 'A' ? 1 : 2;
+        int pos1 = getNumericValue(cor2.charAt(1));
+        int i2 = cor1.charAt(0) == 'A' ? 1 : 2;
+        int pos2 = getNumericValue(cor2.charAt(1));
+
+        if(answers[i1][pos1].equals(answers[i2][pos2])){
+            uncover(cor2); return result=true;
+        }
+        cover(cor1);
+        return result;
+    }
+
+    static class pair{
+        static String guess1;
+        static String guess2;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,10 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 
 
 public class main {
@@ -9,10 +13,10 @@ public class main {
     }
 
 
-//==================================================================================================================
+    //==================================================================================================================
     public main() {
         int difficulty = 0;
-        ArrayList<String> words=null   ;
+        ArrayList<String> words = null;
 
         //String absolutePathtoFile = "C:\\Users\\Dell\\Documents\\Motorola - recruitment task Java\\words.txt";
         String relativePathToFile = "words.txt";
@@ -25,32 +29,44 @@ public class main {
 
         int cnt; //counts user guesses [0-10] in EASY MODE
 
-        Game game1=new Game(words,setDiffculty());
-        game1.printResult();
-        Game game2=new Game(words,setDiffculty());
-        game2.printResult();
+        Game game1 = new Game(words, setDiffculty());
+        game1.printGameTable();
+        game1.printAnswers();
+        String card1, card2;
+        System.out.println("Give first coordinate");
+        card1=getCoordinate();
+        game1.uncover(card1);
+        game1.printGameTable();
+        System.out.println("Give second coordinate");
+        card2=getCoordinate();
+        game1.check(card1,card2);
+
+        game1.printGameTable();
+
+
     }
+
     //==================================================================================================================
     private int setDiffculty() {
-        boolean loop=true;
-        Scanner input=new Scanner(System.in);
-        int guess_cnt=0;
+        boolean loop = true;
+        Scanner input = new Scanner(System.in);
+        int guess_cnt = 0;
         System.out.println("Choose difficulty. Type easy (4 pairs) or hard (8 pairs)");
-        while(loop){
-            String response=input.nextLine();
-            if(response.toLowerCase(Locale.ROOT).equals("easy")) {
+        while (loop) {
+            String response = input.nextLine();
+            if (response.toLowerCase(Locale.ROOT).equals("easy")) {
                 //----------------------------------------------------------------------------------------------;
                 return DIFFICULTY.EASY;
-            }else{
-                if(response.toLowerCase(Locale.ROOT).equals("hard")){
+            } else {
+                if (response.toLowerCase(Locale.ROOT).equals("hard")) {
                     //------------------------------------------------------------------------------------------;
                     return DIFFICULTY.HARD;
-                }else{
+                } else {
                     guess_cnt++;
-                    if (guess_cnt<2) System.out.println("Not recognized.Try again");
-                    else{
+                    if (guess_cnt < 2) System.out.println("Not recognized.Try again");
+                    else {
                         System.out.println("Sorry, can't recognize Your reply. Game level set to EASY");
-                        loop=false;
+                        loop = false;
                     }
                 }
             }
@@ -58,27 +74,43 @@ public class main {
         return DIFFICULTY.EASY;
     }
 
-    ArrayList <String> getWordsArray(String path) throws  IOException {
-        FileInputStream fis= new FileInputStream(path);
-        ArrayList <String> words=new ArrayList<String>();
+    ArrayList<String> getWordsArray(String path) throws IOException {
+        FileInputStream fis = new FileInputStream(path);
+        ArrayList<String> words = new ArrayList<String>();
         int c;
-        c= fis.read();
-        if(c==-1) return null;
+        c = fis.read();
+        if (c == -1) return null;
         //-------------------------------------------------------
-        String w ="";
-        do{
-            if(c=='\r'){
+        String w = "";
+        do {
+            if (c == '\r') {
                 words.add(w);
                 fis.skip(1); // ommiting "\n" character
-                w="";
-            }else{
-                w+=(char)c;
+                w = "";
+            } else {
+                w += (char) c;
 
             }
-            c= fis.read();
-        } while (c!=-1);
+            c = fis.read();
+        } while (c != -1);
         return words;
     }
-}
 
+    String getCoordinate() {
+        boolean correct = false;
+        String input = "";
+        while (!correct) {
+            Scanner sc = new Scanner(System.in);
+            input = sc.nextLine();
+            String cor_pt = "^\\w\\d$";
+            Pattern pt = Pattern.compile(cor_pt);
+            Matcher mt = pt.matcher(input);
+            if (mt.matches()) correct=true;
+            else {
+                System.out.println("Coordinates not recognized. Use Only A or B and digits 1-8");
+            }
+        }
+        return input.toUpperCase(Locale.ROOT);
+    }
+}
 
